d0604411f3bc38c648808b7a66dd1958d6597ba1
coursemdetw <course@mde.tw>
2016-02-21 19:06:41 +0800

    kick off 2016spring

--- content/pages/yen_pages_1.md
+++ content/pages/yen_pages_1.md
@@ -2,5 +2,8 @@ Title: About
 
 ## CAD 實驗室
 
-CAD Lab 為台灣國立虎尾科技大學, 機械設計工程系, 電腦輔助設計實驗室, 全稱為 Computer Aided Design Laboratory (縮寫為 CADLab), Department of Mechanical Design Engineering, National Formosa University, Taiwan.
+CAD Lab (CADLab) 為台灣國立虎尾科技大學, 機械設計工程系, 電腦輔助設計實驗室, 全稱為 Computer Aided Design Laboratory (縮寫為 CADLab), Department of Mechanical Design Engineering, National Formosa University, Taiwan.
 
+## 伺服主機
+
+<http://101.mde.tw>
--- publishconf.py
+++ publishconf.py
@@ -12,12 +12,9 @@ from pelicanconf import *
 
 # 因為 publishconf.py 在 pelicanconf.py 之後, 因此若兩處有相同變數的設定, 將以較後讀入的 publishconf.py 中的設定為主.
 
-# 將所有靜態 html 檔案移到 blog 子目錄
-#SITEURL = 'http://cad-lab.github.io/blog/post'
-# 以下利用 Github CNAME 設定轉換 domain name
-# 特別注意 http://cad-lab.github.io/blog 與 http://cadlab.mde.tw 網址的跳轉 (Github)
+# 請注意, 為了在近端讓 Tipue search 傳回的搜尋結果連結正確, 必須使用 ./
 SITEURL = 'http://cadlab.mde.tw/post'
-# 此設定用於將資料送到 gh-pages, 因此使用絕對 URL 設定
+# 此設定用於近端靜態網頁查驗, 因此使用相對 URL
 RELATIVE_URLS = False
 # 為了要讓 local 與 gh-pages 上都能夠使用 Tipue search, 可能要採用不同的 theme
 THEME = 'theme/pelican-bootstrap3'
@@ -40,11 +37,11 @@ DISQUS_SITENAME = "cadlab"
 # 設定網誌以 md 檔案建立的 file system date 為準, 無需自行設定
 DEFAULT_DATE = 'fs'
 
-# 遠端的 code hightlight
+# 近端的 code hightlight
 MD_EXTENSIONS = ['fenced_code', 'extra', 'codehilite(linenums=True)']
 
 # 若要依照日期存檔呼叫
-#ARTICLE_URL = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+#ARTICLE_URL = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
 #ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
 PAGE_URL = 'pages/{slug}/'
 PAGE_SAVE_AS = 'pages/{slug}/index.html'
